28f388ad292a4a32f7f82966ecf1d816c203d402
ils-harshn <h.narwariya@thesynapses.com>
2023-05-25 17:53:21 +0530

    Admin panel searches

--- songs/admin.py
+++ songs/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import Song, Artist, Album, Playlist, SongReaction, Genre
 
-admin.site.register(Song)
-admin.site.register(Album)
-admin.site.register(Artist)
+class SongAdmin(admin.ModelAdmin):
+    search_fields = ['original_name']
+
+class AlbumAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'code']
+
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+admin.site.register(Song, SongAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Genre, GenreAdmin)
 admin.site.register(Playlist)
 admin.site.register(SongReaction)
-admin.site.register(Genre)
