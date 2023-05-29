627e8fd3ea3747b7651c398365eb511831d13e62
ils-harshn <h.narwariya@thesynapses.com>
2023-05-29 13:08:13 +0530

    Fixed Scroll Bar Risks

--- src/pages/LikedSong/index.js
+++ src/pages/LikedSong/index.js
@@ -1,27 +1,44 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import "./index.scss";
 import { useEffect, useState } from "react";
-import * as likedSongsActions from "../../store/actions/likedSongsActions"
+import { getLikedSongsApi } from "../../Api";
 
 const LikedSongPage = () => {
-    const dispatch = useDispatch();
-    const likedSongs = useSelector((reducers) => reducers.likedSongsReducers)
-    const [songsList, setSongsList] = useState([]);
+    const [songsData, setSongsData] = useState({
+        count: 0,
+        next: null,
+        previous: null,
+        results: [],
+    });
     const data = useSelector((reducers) => reducers.loginReducer);
     const [page, setPage] = useState(1);
     const [fetchingMore, setFetchingMore] = useState(false);
 
+    const fetchSong = async () => {
+        try {
+            setFetchingMore(true)
+            let songsApiData = await getLikedSongsApi(data.user.token, page);
+            setSongsData((prevSongsData => {
+                return {
+                    count: songsApiData.data.count,
+                    next: songsApiData.data.next,
+                    previous: songsApiData.data.previous,
+                    results: [...prevSongsData.results, ...songsApiData.data.results]
+                }
+            }))
+            setFetchingMore(false)
+        } catch {
+            console.log("Error")
+        }
+    }
+
     useEffect(() => {
-        dispatch({ type: likedSongsActions.INITIATE_LIKED_SONGS, payload: { page, token: data.user.token } });
+        fetchSong()
     }, [page])
 
     useEffect(() => {
-        if (likedSongs.loading == false) {
-            setSongsList(prevSongs => [...prevSongs, ...likedSongs.data.results])
-        }
-    }, [likedSongs])
-
-
+        console.log(songsData)
+    }, [songsData])
 
     return (
         <div className="liked-songs-page">
@@ -43,13 +60,12 @@ const LikedSongPage = () => {
             <div className="liked-songs-main" onScroll={(e) => {
                 const element = e.target;
                 if ((element.scrollTop + element.clientHeight >=
-                    element.scrollHeight) && likedSongs.data.next) {
-                    setFetchingMore(true)
+                    element.scrollHeight) && songsData.next) {
                     setPage(prevPage => prevPage + 1)
                 }
             }}>
                 {
-                    (likedSongs.loading && !fetchingMore) ? <table className="table loading">
+                    (fetchingMore && page == 1) ? <table className="table loading">
                         <thead>
                             <tr>
                                 <th>INDEX</th>
@@ -74,7 +90,7 @@ const LikedSongPage = () => {
                         </tbody>
                     </table> :
 
-                        songsList && songsList.length ?
+                        songsData.results.length ?
                             <table className="table">
                                 <thead>
                                     <tr>
@@ -88,7 +104,7 @@ const LikedSongPage = () => {
                                 </thead>
                                 <tbody>
                                     {
-                                        songsList.map((item, index) => (
+                                        songsData.results.map((item, index) => (
                                             <tr key={index}>
                                                 <th scope="row">{index + 1}</th>
                                                 <td>{item.song.original_name}</td>
@@ -98,6 +114,30 @@ const LikedSongPage = () => {
                                                 <td>{item.song.views}</td>
                                             </tr>
                                         ))
+                                    }
+
+                                    {
+                                        fetchingMore &&
+                                        (
+                                            <>
+                                                <tr className="loading">
+                                                    <th scope="row">00</th>
+                                                    <td>Lorem ipsum dolor</td>
+                                                    <td>Lorem ipsum dolor</td>
+                                                    <td>Lorem</td>
+                                                    <td>0000</td>
+                                                    <td>00</td>
+                                                </tr>
+                                                <tr className="loading">
+                                                    <th scope="row">00</th>
+                                                    <td>Lorem ipsum dolor</td>
+                                                    <td>Lorem ipsum dolor</td>
+                                                    <td>Lorem</td>
+                                                    <td>0000</td>
+                                                    <td>00</td>
+                                                </tr>
+                                            </>
+                                        )
                                     }
                                 </tbody>
                             </table> :
