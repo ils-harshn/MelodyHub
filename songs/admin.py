ef30b0c55482b5b079317b856fb704773bd77022
ils-harshn <h.narwariya@thesynapses.com>
2023-06-01 13:00:20 +0530

    Revert "Created a proper song admin panel" As that was wrong This reverts commit a01276e4cb114670ca29611b4030f3ac8897dae1.

--- songs/admin.py
+++ songs/admin.py
@@ -1,28 +1,15 @@
 from django.contrib import admin
 from .models import Song, Artist, Album, Playlist, SongReaction, Genre, RecentSong
-from django import forms
-
-
-class SongAdminForm(forms.ModelForm):
-    artists = forms.ModelMultipleChoiceField(
-        queryset=Artist.objects.all(),
-        widget=admin.widgets.FilteredSelectMultiple('Artists', False),
-        required=False
-    )
-
-    class Meta:
-        model = Song
-        fields = '__all__'
 
 class SongAdmin(admin.ModelAdmin):
-    form = SongAdminForm
+    search_fields = ['original_name']
+    fields = ["album", "title", "url", "original_name", "views", "genre"]
 
 class AlbumAdmin(admin.ModelAdmin):
     search_fields = ['title', 'code']
 
 class ArtistAdmin(admin.ModelAdmin):
     search_fields = ['name']
-    fields = ["name", "artists_thumbnail", "artists_thumbnail300x300"]
 
 class GenreAdmin(admin.ModelAdmin):
     search_fields = ['name']
