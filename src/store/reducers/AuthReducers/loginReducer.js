aafc65e49883877a3597031300be9f762fd90599
ils-harshn <127953102+ils-harshn@users.noreply.github.com>
2023-07-05 19:27:38 +0530

    Update loginReducer.js

--- src/store/reducers/AuthReducers/loginReducer.js
+++ src/store/reducers/AuthReducers/loginReducer.js
@@ -1,15 +1,7 @@
 import { INITIATE_LOGIN, LOGIN_ERROR, LOGIN_SUCCESS, LOGOUT } from "../../actions/types";
 
 const initailState = {
-    user: {
-        "token": "2fcda4f08cef662426bcdc55c9b9adb1199ab014",
-        "email": "harsh.narwariya11@gmail.com",
-        "first_name": "Harsh",
-        "last_name": "Narwariya",
-        "is_admin": true,
-        "date_joined": "2023-06-16T10:55:04.615595Z",
-        "is_active": true
-    },
+    user: null,
     error: null,
     loading: false,
 }
