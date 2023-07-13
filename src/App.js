1d37086ce46e73c89152678c23a0ec4871581dc0
ils-harshn <127953102+ils-harshn@users.noreply.github.com>
2023-07-13 17:03:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     <ThemeProvider theme={currentTheme === 'light' ? ligththeme : darktheme}>
       <GlobalStyles changedTheme={changedTheme} />
       <ThemeChanger toggleTheme={toggleTheme} currentTheme={currentTheme} />
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route path="login" element={<AuthLayout />}>
