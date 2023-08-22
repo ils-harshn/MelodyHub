f83fe7e30807438ef1c3901ad1912f622af8c93b
ils-harshn <h.narwariya@thesynapses.com>
2023-08-22 18:09:44 +0530

    Added Idol Variations

--- src/components/Idol/Idol.stories.tsx
+++ src/components/Idol/Idol.stories.tsx
@@ -3,16 +3,18 @@ import IdolType from "./Idol.type";
 import { Story } from "@storybook/react";
 
 export default {
-    title: "Components/Idol",
-    Component: Idol,
-}
+  title: "Components/Idol",
+  Component: Idol,
+};
 
 const Template: Story<IdolType> = (args) => <Idol {...args}></Idol>;
 
-export const Variations = Template.bind({})
+export const Variations = Template.bind({});
 
 Variations.args = {
   size: "large",
-}
+};
 
-export const Small = () => <Idol></Idol>
+export const Small = () => <Idol size="small"></Idol>;
+export const Medium = () => <Idol size="medium"></Idol>;
+export const Large = () => <Idol size="large"></Idol>;
