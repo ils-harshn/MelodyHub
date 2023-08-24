f3a731c3e36fbc0d99227acaf66f1640b0fc719c
ils-harshn <h.narwariya@thesynapses.com>
2023-08-24 11:53:28 +0530

    Fixed Some Routes Config

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,9 +3,7 @@ import { makeRoute } from "../utils";
 export const INDEX = makeRoute("/");
 
 // Account Nested
-export const AUTH_LAYOUT = {
-    path: "account",
-};
+export const AUTH_LAYOUT = makeRoute("account");
 
 export const LOGIN = makeRoute("login", AUTH_LAYOUT.path);
 export const FORGET_PASSWORD = makeRoute("forgetpassword", AUTH_LAYOUT.path);
