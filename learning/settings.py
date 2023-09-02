1eae75dd2135452eb63b74c48f56ff2ed882b6a3
ils-harshn <h.narwariya@thesynapses.com>
2023-09-02 18:18:16 +0530

    Removed Cred

--- learning/settings.py
+++ learning/settings.py
@@ -156,6 +156,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'harshverma790932611@gmail.com'
-EMAIL_HOST_PASSWORD = 'axqwwcbfrzvhidkn'
